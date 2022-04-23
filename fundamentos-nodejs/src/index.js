f0cd9dc4a921ae4f5387f0fc13cca5b376173d3a
Guilherme Reis Araújo <guih2127@gmail.com>
2022-04-23 10:28:39 -0300

    Last endpoints of the initial project

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -89,12 +89,45 @@ app.post("/withdraw", verifyIfExistsAccountCpf, (request, response) => {
     description,
     amount,
     created_at: new Date(),
-    type: "credit",
+    type: "debit",
   };
 
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => {
+    return (
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+    );
+  });
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCpf, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.delete("/account", verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
 });
 
 app.use(express.json());
