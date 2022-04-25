b67e41c8be025d6392f2f7db59f9209749ac18a8
Guilherme Reis Araújo <guih2127@gmail.com>
2022-04-24 23:10:25 -0300

    remove upload files

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
+import swaggerUI from "swagger-ui-express";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
